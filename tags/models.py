225099e2d81743d31031d79cc59116fec44bbece
Fikri Ghazi <fikrigha@gmail.com>
2021-08-22 22:24:11 -0400

    add tags

---
+++ tags/models.py
@@ -0,0 +1,17 @@
+from store.models import Product
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    # Generic relationship
+    # You will need 2 things: Type and ID
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
